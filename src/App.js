566481ad81e4d03bae3dec58691cf46f579c1efe
brandontam29 <tam.brandon29@gmail.com>
2022-05-27 17:32:27 -0400

    added packages and changed readme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Layout from "./components/layout/Layout";
 import styles from "./App.css";
 import { Routes, Route } from "react-router-dom";
-import AboutMe from './pages/AboutMePage';
-import ContactMe from './pages/ContactMePage';
-import Games from './pages/GamesPage';
+import AboutMe from "./pages/AboutMePage";
+import ContactMe from "./pages/ContactMePage";
+import Games from "./pages/GamesPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<AboutMe/>}></Route>
-        <Route path='/ContactMe' element={<ContactMe/>}></Route>
-        <Route path='/Games' element={<Games/>}></Route>
+        <Route path="/" element={<AboutMe />}></Route>
+        <Route path="/ContactMe" element={<ContactMe />}></Route>
+        <Route path="/Games" element={<Games />}></Route>
       </Routes>
     </Layout>
   );
