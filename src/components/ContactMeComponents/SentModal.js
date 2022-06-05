a553f526d041539ea48f2b0adb9032fa6702e32e
yoman3215 <luophilip@outlook.com>
2022-06-05 16:17:31 -0400

    lined contact form to emailjs

---
+++ src/components/ContactMeComponents/SentModal.js
@@ -0,0 +1,10 @@
+import styles from "./SentModal.module.css";
+
+const ConfirmationModal = () => {
+  return (
+    <div className={styles.modal}>
+      <h3>Email has been sent successfully</h3>
+    </div>
+  );
+};
+export default ConfirmationModal;
